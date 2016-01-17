739a7cbb6facfdd593a3fa698dc1075750bbb8b6
Tadeas Kriz <tadeas@brightify.org>
2016-01-17 21:16:53 +0100

    Create cuckoo.rb

---
+++ cuckoo.rb
@@ -0,0 +1,16 @@
+class Cuckoo < Formula
+    desc "Command-line tool for generating Cuckoo mock files."
+    homepage "https://github.com/SwiftKit/CuckooGenerator"
+    url "https://github.com/SwiftKit/CuckooGenerator.git", :tag => "0.1.0"
+    head "https://github.com/SwiftKit/CuckooGenerator.git"
+
+    depends_on :xcode => ["7.1", :build]
+    
+    def install
+        system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/CuckooGenerator.dst"
+    end
+    
+    test do
+        system "#{bin}/cuckoo", "version"
+    end
+end
