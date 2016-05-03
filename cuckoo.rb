4ebc44a8c0998a53c3ebe577135625f3f3a2d377
Tadeas Kriz <tadeas@brightify.org>
2016-05-03 12:25:06 +0200

    Update generator version.

--- cuckoo.rb
+++ cuckoo.rb
@@ -1,11 +1,11 @@
 class Cuckoo < Formula
     desc "Command-line tool for generating Cuckoo mock files."
     homepage "https://github.com/SwiftKit/CuckooGenerator"
-    url "https://github.com/SwiftKit/CuckooGenerator.git", :tag => "0.3.0"
+    url "https://github.com/SwiftKit/CuckooGenerator.git", :tag => "0.4.0"
     head "https://github.com/SwiftKit/CuckooGenerator.git"
 
     depends_on "carthage" => :build
-    depends_on :xcode => ["7.1", :build]
+    depends_on :xcode => ["7.3", :build]
     
     def install
         system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/CuckooGenerator.dst"
